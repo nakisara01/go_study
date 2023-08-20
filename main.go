838c3ef30a4fa0df4eecffe6b2787d153b078865
n_heum <nakisara01@ajou.ac.kr>
2023-08-21 01:54:59 +0900

    print, variable

---
+++ main.go
@@ -0,0 +1,37 @@
+// 0. print "Hello, World!"
+
+/* package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")
+}
+*/
+
+
+
+// 1. Using Variable
+
+/* package main
+
+import "fmt"
+
+func main(){
+
+	var a int = 10
+	var s string = "Hi"
+	// var empty float32 = 0 *go에서는 변수(혹은 import 된 모듈 등)를 선언한 후 그 변수를 사용하지 않으면 오류가 발생함.*
+	
+	fmt.Println(a)
+	fmt.Println(s)
+} 
+*/
+
+package main
+
+import "fmt"
+
+func main(){
+
+}
